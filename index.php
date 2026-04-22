ab79a8127d39daa021e8090f02e481bbf614c836
Alex <megacyberalex@gmail.com>
2026-04-22 17:29:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-$googleUrl = "https://google.com";
+$googleUrl = "https://script.google.com/macros/s/AKfycbwGojJkYXpCh0JOjS1TspA4_1QapHnuXDDCIDnpGOC7aqbi3oStsAXVHwYFILlkKALw/exec";
 $url = $googleUrl . "?" . $_SERVER['QUERY_STRING'];
 echo file_get_contents($url);
